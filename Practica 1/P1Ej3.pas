c53dcc3e9a85e6d48439355534a5735ee06b3d79
pablocabe <pablocabe0@gmail.com>
2025-03-17 14:06:07 -0300

    .

---
+++ Practica 1/P1Ej3.pas
@@ -0,0 +1,16 @@
+program P1Ej3;
+
+
+{
+Realizar un programa que presente un menú con opciones para:
+a. Crear un archivo de registros no ordenados de empleados y completarlo con
+datos ingresados desde teclado. De cada empleado se registra: número de
+empleado, apellido, nombre, edad y DNI. Algunos empleados se ingresan con
+DNI 00. La carga finaliza cuando se ingresa el String ‘fin’ como apellido.
+b. Abrir el archivo anteriormente generado y
+i. Listar en pantalla los datos de empleados que tengan un nombre o apellido
+determinado, el cual se proporciona desde el teclado.
+ii. Listar en pantalla los empleados de a uno por línea.
+iii. Listar en pantalla los empleados mayores de 70 años, próximos a jubilarse.
+
+}
