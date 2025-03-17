e8777d01e473412f3df303c355e3e8707636709d
pablocabe <pablocabe0@gmail.com>
2025-03-17 14:02:37 -0300

    .

--- Practica 1/P1Ej2.pas
+++ Practica 1/P1Ej2.pas
@@ -3,6 +3,7 @@ program P1Ej2;
 type
     archivoNumeros = file of integer;
 
+
 procedure crearArchivo (var archNumeros: archivoNumeros);
 var
     num: integer;
@@ -57,7 +58,7 @@ end.
 
 {
 Realizar un algoritmo, que utilizando el archivo de números enteros no ordenados
-creado en el ejercicio 1, informe por pantalla cantidad de números menores a 1500 y el
+creado en el ejercicio 1, informe por pantalla la cantidad de números menores a 1500 y el
 promedio de los números ingresados. El nombre del archivo a procesar debe ser
 proporcionado por el usuario una única vez. Además, el algoritmo deberá listar el
 contenido del archivo en pantalla.
