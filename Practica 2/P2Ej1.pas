524988ccd9f2eb047ac2c2b77a3eeac81027fac3
pablocabe <pablocabe0@gmail.com>
2025-03-18 15:52:11 -0300

    .

---
+++ Practica 2/P2Ej1.pas
@@ -0,0 +1,17 @@
+program P2Ej1;
+
+
+{
+Una empresa posee un archivo con información de los ingresos percibidos por diferentes
+empleados en concepto de comisión, de cada uno de ellos se conoce: código de empleado,
+nombre y monto de la comisión. La información del archivo se encuentra ordenada por
+código de empleado y cada empleado puede aparecer más de una vez en el archivo de
+comisiones.
+
+Realice un procedimiento que reciba el archivo anteriormente descrito y lo compacte. En
+consecuencia, deberá generar un nuevo archivo en el cual, cada empleado aparezca una
+única vez con el valor total de sus comisiones.
+
+NOTA: No se conoce a priori la cantidad de empleados. Además, el archivo debe ser
+recorrido una única vez.
+}
