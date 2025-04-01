61a9b7098541669568da2cb022ed4c21b44b02b7
pablocabe <pablocabe0@gmail.com>
2025-04-01 13:06:22 -0300

    .

--- Practica 2/P2Ej8.pas
+++ Practica 2/P2Ej8.pas
@@ -1,6 +1,69 @@
 program P2Ej8;
 
+type
 
+    registroCliente = record
+        codigo: integer;
+        nombre: string;
+        apellido: string;
+    end;
+
+
+    registroMaestro = record
+        cliente: registroCliente;
+        anio: integer;
+        mes: integer;
+        dia: integer;
+        monto: real;
+    end;
+
+    archivoMaestro = file of registroMaestro;
+
+procedure informarArchivoMaestro (var archM: archivoMaestro);
+var
+    regM: registroMaestro;
+    totalMensual, totalAnual, totalEmpresa: real;
+    codigoActual, anioActual, mesActual, cantVentas: integer;
+begin
+    reset (archM);
+    totalEmpresa := 0;
+    
+    while (not EOF (archM)) do begin
+        read (archM, regM);
+        codigoActual := regM.cliente.codigo;
+        writeln ('El cliente es ', regM.cliente.nombre , ' ', regM.cliente.apellido, '. Su codigo es: ', regM.cliente.codigo);
+
+        while (regM.cliente.codigo = codigoActual) do begin
+            anioActual := regM.cliente.anio;
+            totalAnual := 0;
+            while (regM.cliente.codigo = codigoActual) and (anioActual = regM.cliente.anio) do begin
+                mesActual := regM.cliente.mes;
+                totalMensual := 0;
+                cantVentas := 0;
+                while (regM.cliente.codigo = codigoActual) and (anioActual = regM.cliente.anio)
+                and (mesActual = regM.cliente.mes) do begin
+                    totalEmpresa := totalEmpresa + regM.monto;
+                    totalMensual := totalMensual + regM.monto;
+                    cantVentas := cantVentas + 1;
+                    read (archM, regM);
+                end;
+                totalAnual := totalAnual + totalMensual;
+                writeln ('En el mes ', mesActual, ' compro un total de : ', cantVentas);
+            end;
+            writeln ('En el anio ', anioActual, ' se gasto ', totalAnual);
+        end;
+    end;
+    writeln ('El monto total de ventas obtenido por la empresa es: ', totalEmpresa);
+    close (archM);
+end;
+
+
+var
+    archM: archivoMaestro;
+begin
+    crearArchivoMaestro (archM); // se dispone
+    informarArchivoMaestro (archM);
+end.
 
 {
 Se cuenta con un archivo que posee información de las ventas que realiza una empresa a
