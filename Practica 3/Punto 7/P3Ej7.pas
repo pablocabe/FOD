e0965196dc3155266a6bf8a7698035c8600f2abd
pablocabe <pablocabe0@gmail.com>
2025-04-29 12:25:37 -0300

    .

--- Practica 3/Punto 7/P3Ej7.pas
+++ Practica 3/Punto 7/P3Ej7.pas
@@ -18,12 +18,99 @@ i. Implemente una variante de este procedimiento de compactación del
 archivo (baja física) donde el archivo se trunque una sola vez.
 }
 
-program P3Ej7;
+program P3Ej7; // Me falta revisar el b y hacer el i
 
 type
 
-var
+    registroAve = record
+        codigo: integer;
+        nombre: string;
+        familia: string;
+        descripcion: string;
+        zona: string;
+    end;
 
+    archivoMaestro = file of registroAve; // No esta ordenado
+
+
+procedure crearArchivoMaestro (var archM: archivoMaestro); // Se dispone
+
+
+procedure bajaLogica (var archM: archivoMaestro; codigoTeclado: integer);
+var
+    regM: registroAve;
+    existe: boolean;
 begin
-    
+    reset (archM);
+    existe := false;
+    while (not EOF (archM)) and (not existe) do begin
+        read (archM, regM);
+        if (archM.codigo = codigoTeclado) then begin
+            existe := true;
+            seek (archM, filePos(archM)-1);
+            regM.codigo := regM.codigo * -1;
+            write (archM, regM);
+        end;
+    end;
+    if (existe) then
+        writeln ('Se realizo la baja logica del ave con codigo ', codigoTeclado)
+    else
+        writeln ('No se encontro el ave con el codigo ', codigoTeclado);
+    close (archM);
+end;
+
+
+procedure compactarArchivo (var archM: archivoMaestro); // Mas directo?
+var
+    regM: registroAve;
+    pos: integer;
+begin
+    reset (archM);
+    while (not EOF (archM)) do begin
+        read (archM, regM);
+        if (regM.codigo < 0) then begin
+            pos := filePos(archM-1);
+            seek (archM, fileSize(archM-1));
+            read (archM, regM);
+            truncate (archM);
+            seek (archM, pos);
+            write (archM, regM);
+        end;
+    end;
+    close (archM);
+end;
+
+
+{procedure compactarArchivo (var archM: archivoMaestro);
+var
+    regM: registroAve;
+    pos: integer;
+begin
+    reset (archM);
+    while (not EOF (archM)) do begin
+        read (archM, regM);
+        if (regM.codigo < 0) then begin
+            pos := filePos(archM-1);
+            seek (archM, fileSize(archM-1));
+            read (archM, regM);
+            seek (archM, pos);
+            write (archM, regM);
+            seek (archM, fileSize(archM-1));
+            truncate (archM);
+            seek (archM, pos);
+        end;
+    end;
+    close (archM);
+end;}
+
+
+var
+    archM: archivoMaestro;
+    codigoTeclado: integer;
+begin
+    crearArchivoMaestro (archM) // Se dispone
+    writeln ('Ingrese el codigo de la ave a borrar');
+    readln (codigoTeclado);
+    bajaLogica (archM, codigoTeclado);
+    compactarArchivo (archM);
 end.
---
+++ Practica 3/Punto 8/P3Ej8.pas
@@ -0,0 +1,31 @@
+{
+Se cuenta con un archivo con información de las diferentes distribuciones de linux 
+existentes. De cada distribución se conoce: nombre, año de lanzamiento, número de 
+versión  del  kernel,  cantidad  de  desarrolladores  y  descripción.  El  nombre  de  las 
+distribuciones no puede repetirse. Este archivo debe ser mantenido realizando bajas 
+lógicas y utilizando la técnica de reutilización de espacio libre llamada lista invertida. 
+Escriba  la  definición  de  las  estructuras  de  datos  necesarias  y  los  siguientes 
+procedimientos:
+
+a.  BuscarDistribucion:  módulo  que  recibe  por  parámetro  el  archivo,  un 
+nombre de distribución y devuelve la posición dentro del archivo donde se 
+encuentra  el registro correspondiente a la distribución dada (si existe) o 
+devuelve -1 en caso de que no exista.. 
+
+b.  AltaDistribucion: módulo que recibe como parámetro el archivo y el registro 
+que contiene los datos de una nueva distribución, y se encarga de agregar 
+la distribución   al archivo reutilizando espacio disponible en caso de que 
+exista. (El control de unicidad lo debe realizar utilizando el módulo anterior). 
+En caso de que la distribución que se quiere agregar ya exista se debe 
+informar “ya existe la distribución”. 
+
+c.  BajaDistribucion:  módulo  que  recibe  como  parámetro  el  archivo  y  el 
+nombre de una distribución, y se encarga de dar de baja lógicamente la 
+distribución  dada.  Para  marcar una distribución como borrada se debe 
+utilizar el campo cantidad de desarrolladores para mantener actualizada 
+la lista invertida. Para verificar que la distribución a borrar exista debe utilizar 
+el  módulo  BuscarDistribucion.  En  caso  de  no  existir  se  debe  informar 
+“Distribución no existente”.
+}
+
+program P3Ej8;
