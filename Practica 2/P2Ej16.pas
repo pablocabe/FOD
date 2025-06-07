0ee14fbe3e95b3f82b105cfe0f48ed9c5835f9e6
pablocabe <pablocabe0@gmail.com>
2025-06-07 16:24:24 -0300

    .

--- Practica 2/P2Ej16.pas
+++ Practica 2/P2Ej16.pas
@@ -16,6 +16,8 @@ Nota: Todos los archivos están ordenados por fecha y código de semanario. No s
 ventas de semanarios si no hay ejemplares para hacerlo.
 }
 
+
+// Me falta informar y cambiar tantas variables por 2 registrosDetalle
 program P2Ej16;
 
 const
@@ -65,6 +67,92 @@ begin
     end;
 end;
 
+
+procedure leerDetalle (var archD: archivoDetalle; var regD: registroDetalle);
+begin
+    if (not EOF (archD)) then
+        read (archD, regD)
+    else
+        regD.fecha := valorAlto;
+end;
+
+
+procedure buscarMinimo (var vectorD: vectorDetalles; var vectorR: vectorRegistros; var minimo: registroDetalle);
+var
+    i, pos: subrango;
+begin
+    minimo.fecha := valorAlto;
+    for i := 1 to dF do begin
+        if (vectorR[i].fecha < minimo.fecha) or 
+        ((vectorR[i].fecha = minimo.fecha) and (vectorR[i].codigoSemanario < minimo.codigoSemanario)) then begin
+            minimo := vectorR[i];
+            pos := i;
+        end;
+    end;
+    if (minimo.fecha <> valorAlto) then
+        leerDetalle (vectorD[pos], vectorR[pos]);
+end;
+
+
+procedure actualizarArchivoMaestro (var archM: archivoMaestro; var vectorD: vectorDetalles);
+var
+    i: subrango;
+    minimo: registroDetalle;
+    vectorR: vectorRegistros;
+    fechaMaxVentas, codigoMaxVentas, fechaMinVentas, codigoMinVentas, cantMax, cantMin: integer;
+begin
+    reset (archM);
+    for i:= 1 to dF do begin
+        reset (vectorD[i]);
+        leerDetalle (vectorD[i], vectorR[i]);
+    end;
+    fechaMaxVentas := 0;
+    fechaMinVentas := 0;
+    codigoMaxVentas := 0;
+    codigoMinVentas := 0;
+    cantMax := 0;
+    cantMin := 0;
+
+    leerMaestro (archM, regM);
+    buscarMinimo (vectorD, vectorR, minimo);
+    while (minimo.fecha <> valorAlto) do begin
+
+        while (minimo.fecha <> regM.fecha) do
+            read (archM, regM);
+
+        while (minimo.fecha = regM.fecha) do begin
+            while (minimo.codigoSemanario <> regM.codigoSemanario) do
+                read (archM, regM);
+
+            while (minimo.fecha = regM.fecha) and (minimo.codigoSemanario = regM.codigoSemanario) do begin
+                if (regM.totalEjemplares > minimo.cantEjemplaresVendidos) then begin
+                    regM.totalEjemplaresVendidos := regM.totalEjemplaresVendidos + minimo.cantEjemplaresVendidos;
+                    regM.totalEjemplares := regM.totalEjemplares - minimo.cantEjemplaresVendidos;
+                end;
+                buscarMinimo (vectorD, vectorR, minimo);
+            end;
+
+            if (regM.totalEjemplaresVendidos > cantMax) then begin
+                fechaMaxVentas := regM.fecha;
+                codigoMaxVentas := regM.codigoSemanario;
+            end;
+
+            if (regM.totalEjemplaresVendidos < cantMin) then begin
+                fechaMinVentas := regM.fecha;
+                codigoMinVentas := regM.codigoSemanario;
+            end;
+
+            seek (archM, filePos(archM)-1);
+            write (archM, regM);
+            read (archM, regM);
+        end;
+
+    end;
+
+    close (archM);
+    for i:= 1 to dF do
+        close (vectorD[i]);
+end;
 
 var
     archM: archivoMaestro;
